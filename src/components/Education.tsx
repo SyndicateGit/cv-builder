79a2e810b747722ecde16052093d98851f2cd06d
Raymond Zeng <shilei.zeng98@outlook.com>
2023-11-28 17:41:48 -0500

    Add education component

---
+++ src/components/Education.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import InputGroup from './InputGroup';
+import '../styles/Education.css';
+
+// type props = {
+//   onChange:(e:any) => void; //TODO: Fix event type
+//   fullName: string;
+//   email: string;
+//   phoneNumber: string;
+//   linkedInLink: string;
+//   githubLink: string;
+// }
+
+export default function Education(props:any){
+
+  return (
+    <>
+      <form action="" className='education-info'>
+        <h2>Education</h2>
+        
+      </form>
+    </>
+  )
+}
