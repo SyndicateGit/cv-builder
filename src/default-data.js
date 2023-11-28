538c92d9677bdb56b857eea280156daa7080610f
Raymond Zeng <shilei.zeng98@outlook.com>
2023-11-28 16:53:00 -0500

    Added default data for resume as js object

---
+++ src/default-data.js
@@ -0,0 +1,13 @@
+import { v4 as uuid } from 'uuid';
+
+const defaultData = {
+  BasicInfo:{
+    fullName: "Raymond Zeng",
+    email: "shilei.zeng98@outlook.com",
+    phoneNumber: "(647) 919-1311",
+    linkedInLink: "linkedin.com/in/shilei-zeng",
+    githubLink: "github.com/SyndicateGit"
+  }
+}
+
+export default defaultData;
