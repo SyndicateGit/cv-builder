442a9b0f603f9a137e71bd09835d4133b0fb30e4
Raymond Zeng <shilei.zeng98@outlook.com>
2023-11-29 08:38:07 -0500

    Added display drop down state handling

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -15,18 +15,24 @@ import { mdiArrowDownDropCircleOutline } from '@mdi/js';
 // }
 
 export default function Education(props:any){
+  const [displayDropDown, setDisplayDropDown] = React.useState('hide');
 
+  function handleDisplayDropDown(){
+    setDisplayDropDown(displayDropDown === "hide"? "show":"hide");
+  }
   return (
     <>
       <form action="" className='education-info'>
         <div className='drop-down'>
           <h2>Education</h2>
-          <button className='drop-down-button'>
+          <button className="drop-down-button" 
+          type="button"
+          onClick={handleDisplayDropDown}>
             <Icon path={mdiArrowDownDropCircleOutline}
             size={1}
             />
           </button>
-          <div className='hide'>
+          <div className={displayDropDown}>
             <InputGroup onChange={function (e: any): void {
               throw new Error('Function not implemented.');
             } } type={''} id={''} labelText={''} placeholder={''} value={''} dataKey={''}/>
