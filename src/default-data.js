c43140b1c5a920a22121aeece9492b0e642a058f
Raymond Zeng <shilei.zeng98@outlook.com>
2023-11-29 08:27:04 -0500

    Add default education info

--- src/default-data.js
+++ src/default-data.js
@@ -7,7 +7,25 @@ const defaultData = {
     phoneNumber: "(647) 919-1311",
     linkedInLink: "linkedin.com/in/shilei-zeng",
     githubLink: "github.com/SyndicateGit"
-  }
+  },
+  EducationInfo1:{
+    degree: "BCS for Computer Science",
+    school: "University of Windsor",
+    city: "Windsor",
+    province: "Ontario",
+    startDate: "05/01/2023",
+    endDate: "04/30/2025",
+    gpa: "4.0/4.0"
+  },
+  EducationInfo2:{
+    degree: "BSc for Molecular Genetics and Microbiology",
+    school: "University of Toronto",
+    city: "Toronto",
+    province: "Ontario",
+    startDate: "09/01/2016",
+    endDate: "04/30/2020",
+    gpa: "3.6/4.0"
+  },
 }
 
 export default defaultData;
