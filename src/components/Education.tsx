dfb55a8c5680073b49444441549d9720b30d0eaa
Raymond Zeng <shilei.zeng98@outlook.com>
2023-11-29 08:27:52 -0500

    Imported @mdi/react mdi/js for drop down icon

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import InputGroup from './InputGroup';
 import '../styles/Education.css';
+import Icon from '@mdi/react';
+import { mdiArrowDownDropCircleOutline } from '@mdi/js';
+
 
 // type props = {
 //   onChange:(e:any) => void; //TODO: Fix event type
@@ -16,7 +19,20 @@ export default function Education(props:any){
   return (
     <>
       <form action="" className='education-info'>
-        <h2>Education</h2>
+        <div className='drop-down'>
+          <h2>Education</h2>
+          <button className='drop-down-button'>
+            <Icon path={mdiArrowDownDropCircleOutline}
+            size={1}
+            />
+          </button>
+          <div className='hide'>
+            <InputGroup onChange={function (e: any): void {
+              throw new Error('Function not implemented.');
+            } } type={''} id={''} labelText={''} placeholder={''} value={''} dataKey={''}/>
+          </div>
+        </div>
+
         
       </form>
     </>
