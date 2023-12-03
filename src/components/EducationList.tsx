fdf9db0408bcca65bca8bf35c5582118f3961612
Raymond Zeng <shilei.zeng98@outlook.com>
2023-12-03 17:30:51 -0500

    Added RenderEducationList

--- src/components/EducationList.tsx
+++ src/components/EducationList.tsx
@@ -15,13 +15,32 @@ type props = {
   educationList: education[]
 }
 
+const RenderEducationList: React.FC<{list:education[]}> = ({list}) => {
+  // TODO: Figure out why <EducationList> wants list function method implementation. Find a workaround.
+  if(!list){
+    return <div></div>
+  }
+  return(
+    <>
+      {
+        list.map((education) => {
+            return <div key={education.id}>{education.school}</div>
+        })
+      }
+    </>
+  );
+}
+
+
 //TODO: Implement EducationList Component
 export default function EducationList(props:props){
 
   return (
     <>
       <div className='education-list'>
-
+        <RenderEducationList 
+        list={props.educationList}        
+        />
       </div>
     </>
   )
