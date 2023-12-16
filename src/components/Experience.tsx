d4daed2d56f1760dedf98efa09ae08d85c13ca83
Raymond Zeng <shilei.zeng98@outlook.com>
2023-12-16 16:10:03 -0500

    Added input fields to Experience
    Still need to hook it up to props down the road.

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -20,6 +20,78 @@ export default function Experience(props:any){
             />
           </button>
           <div className={props.displayDropDown.display + " input-field"}>
+            <InputGroup 
+            onChange={function (e: any): void {
+              throw new Error('Function not implemented.');
+            } } 
+            type={'text'} 
+            id={'jobTitle'} 
+            labelText={'Job Title'} 
+            placeholder={'Enter Job Title'} 
+            value={''} 
+            dataKey={'jobTitle'}
+            />
+            <InputGroup 
+            onChange={function (e: any): void {
+              throw new Error('Function not implemented.');
+            } } 
+            type={'text'} 
+            id={'company'} 
+            labelText={'Company'} 
+            placeholder={'Enter Company'} 
+            value={''} 
+            dataKey={'company'}
+            />
+            <InputGroup 
+            onChange={function (e: any): void {
+              throw new Error('Function not implemented.');
+            } } 
+            type={'text'} 
+            id={'city'} 
+            labelText={'City'} 
+            placeholder={'Enter city'} 
+            value={''} 
+            dataKey={'city'}
+            />
+            <InputGroup 
+            onChange={function (e: any): void {
+              throw new Error('Function not implemented.');
+            } } 
+            type={'text'} 
+            id={'province'} 
+            labelText={'Province'} 
+            placeholder={'Enter Province'} 
+            value={''} 
+            dataKey={'province'}
+            />
+            <InputGroup 
+            onChange={function (e: any): void {
+              throw new Error('Function not implemented.');
+            } } 
+            type={'date'} 
+            id={'startDate'} 
+            labelText={'Start Date'} 
+            placeholder={'Start Date'} 
+            value={''} 
+            dataKey={'startDate'}
+            />
+            <InputGroup 
+            onChange={function (e: any): void {
+              throw new Error('Function not implemented.');
+            } } 
+            type={'date'} 
+            id={'endDate'} 
+            labelText={'End Date'} 
+            placeholder={'End Date'} 
+            value={''} 
+            dataKey={'endDate'}
+            />
+            <div className='input-group'>
+              <label htmlFor="description">
+                <span className='label-text'>Description</span>
+              </label>
+              <textarea name="description" id="description" placeholder='Main Tasks'></textarea>
+            </div>
             <button  type='button'>
               <Icon path={mdiContentSaveCheckOutline} size={1} />
               <span className='Add'>Add</span>
